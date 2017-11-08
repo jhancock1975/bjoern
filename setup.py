9bf2f35df5c58f42136433e7efbb2701031b6c48
jhancock1975 <jhancock1975@gmail.com>
2017-11-08 13:32:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ bjoern_extension = Extension(
     '_bjoern',
     sources       = SOURCE_FILES,
     libraries     = ['ev'],
-    include_dirs  = ['http-parser', '/usr/include/libev'],
+    include_dirs  = ['http-parser', '/usr/include/libev', 'include'],
     define_macros = [('WANT_SENDFILE', '1'),
                      ('WANT_SIGINT_HANDLING', '1')],
     extra_compile_args = ['-std=c99', '-fno-strict-aliasing', '-fcommon',
                           '-fPIC', '-Wall', '-Wextra', '-Wno-unused-parameter',
-                          '-Wno-missing-field-initializers', '-g', '-L ./lib', '-I ./include']
+                          '-Wno-missing-field-initializers', '-g', '-L ./lib']
 )
 
 setup(
